936f8ab42b8d99ddb4a5464231ee05c627f8c643
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-09-08 14:49:58 -0400

    main module-classes

---
+++ lib/dbscan.rb
@@ -0,0 +1,101 @@
+## https://github.com/shiguodong/dbscan (fork)
+
+require 'distance_measures'
+
+require 'geocoder'
+
+class Array
+	def haversine_distance2(n)
+		return Geocoder::Calculations::distance_between( self, n )
+	end
+end
+
+module DBSCAN
+
+	class Clusterer
+		attr_accessor :points, :options, :clusters
+
+		def initialize( points, options = {} )
+			options[:distance] = :euclidean_distance if !options[:distance]
+			@points, @options, @clusters = points.map { |e| Point.new(e) }, options, {-1 => []}
+
+			clusterize!
+		end
+
+		def clusterize!
+			current_cluster = -1
+			@points.each do |point|
+				if !point.visited?
+					point.visit!
+					neighbors = inmediate_neighbors( point )
+					if neighbors.size >= options[:min_points]
+						current_cluster += 1
+						point.cluster = current_cluster
+						cluster = [point].push( add_connected( neighbors, current_cluster ))
+						clusters[current_cluster] = cluster.flatten
+					else
+						clusters[-1].push( point )
+					end
+				end
+			end
+
+		end
+
+		def results
+			hash = {}
+			@clusters.dup.each { |cluster_index, value| hash[cluster_index] = value.flatten.map(&:items) if !value.flatten.empty? }
+			hash
+		end
+
+		def inmediate_neighbors( point )
+			neighbors = []
+			@points.each do |p|
+				if p.items != point.items
+					d = eval("point.items.#{options[:distance]}( p.items )")
+					neighbors.push( p ) if d < options[:epsilon]
+				end
+			end
+			neighbors
+		end
+
+		def add_connected( neighbors, current_cluster )
+			cluster_points = []
+			neighbors.each do |point|
+				if !point.visited?
+					point.visit!
+					new_points = inmediate_neighbors(point)
+
+					if new_points.size >= options[:min_points]
+						new_points.each do |p|
+							if !neighbors.include?(p)
+								neighbors.push( p )
+							end
+						end
+					end
+				end
+
+				if !point.cluster
+					cluster_points.push( point )
+					point.cluster = current_cluster
+				end
+			end
+
+			cluster_points
+		end
+	end
+
+	class Point
+		attr_accessor :items, :cluster, :visited
+		define_method(:visited?) { @visited }
+		define_method(:visit!) { @visited = true }
+		def initialize( point )
+			@items, @cluster, @visited = point, nil, false
+		end
+	end
+
+	def DBSCAN( * args )
+		Clusterer.new( *args )
+	end
+end
+
+include DBSCAN
